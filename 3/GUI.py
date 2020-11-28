ec9801a3961aa480a8232240d2f253a95d537ba0
Zhiyuan Cao <31185624+czy0538@users.noreply.github.com>
2020-11-28 17:06:56 +0800

    删除部分冗余代码

--- 3/GUI.py
+++ 3/GUI.py
@@ -64,17 +64,3 @@ class MyMainForm(QMainWindow, Ui_Form):
     def __init__(self, parent=None):
         super(MyMainForm, self).__init__(parent)
         self.setupUi(self)
-
-
-def main():
-    app = QApplication(sys.argv)
-    # 初始化
-    myWin = MyMainForm()
-    # 将窗口控件显示在屏幕上
-    myWin.show()
-    # 程序运行，sys.exit方法确保程序完整退出。
-    sys.exit(app.exec_())
-
-
-if __name__ == "__main__":
-    main()
--- 3/main.py
+++ 3/main.py
@@ -1,6 +1,5 @@
 import sys
 from PyQt5.QtWidgets import QApplication
-import dataBase
 import GUI
 
 def main():
